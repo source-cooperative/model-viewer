fc450b082f35dc37351aa625343ddfa2a17de6e9
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2026-02-09 14:20:28 -0800

    Display error message if model file is too large/bad response

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import prettyBytes from "pretty-bytes";
   import { onMount } from "svelte";
   import { page } from "$app/state";
 
@@ -7,21 +8,22 @@
   const MAX_CONTENT_LENGTH_MB = 200;
 
   let searchParams: URLSearchParams = page.url.searchParams;
-  let urlParam: string | null = searchParams.get("url");
+  let urlParam: string | null = $state(searchParams.get("url"));
+  let errorMessage: string | null = $state(null);
 
   const validateModelUrl = async (url: string) => {
     const response = await fetch(url, { method: "HEAD" });
     if (!response.ok) {
-      console.error(`Model file URL is not valid: ${url}. Status: ${response.status}.`);
+      errorMessage = `Model file URL is not valid: ${url}. Status: ${response.status}.`;
+      console.error(errorMessage);
       return false;
     }
 
     const contentLength = parseInt(response.headers.get("Content-Length") ?? "-1");
     const maxContentLength = MAX_CONTENT_LENGTH_MB * 1024 * 1024;
     if (contentLength > maxContentLength) {
-      console.error(
-        `Model file is too large: ${contentLength} bytes. Max allowed: ${maxContentLength} bytes.`
-      );
+      errorMessage = `Model file is too large to display (${prettyBytes(contentLength).toLocaleUpperCase()}).`;
+      console.error(errorMessage);
       return false;
     }
 
@@ -44,22 +46,29 @@
   <title>Model Viewer</title>
 </svelte:head>
 
-{#if urlParam}
+{#if urlParam && !errorMessage}
   <div class="h-screen w-screen">
     <ModelViewer modelUrl={urlParam} />
   </div>
 {:else}
   <div class="h-screen w-screen flex flex-col items-center justify-center">
     <div class="max-w-200 p-8">
-      <p class="text-3xl font-semibold mb-4">Valid 3D model file URL required</p>
-      <p class="text-xl">
-        Please provide a valid 3D model file URL via query parameter <code
-          class="bg-source-200 dark:bg-source-800 text-source-800 dark:text-source-200 p-1 rounded-sm"
-        >
-          url
-        </code>. Supported model formats include GLB, GLTF, OBJ, and STL.
-        Maximum allowed file size is {MAX_CONTENT_LENGTH_MB} MB.
-      </p>
+      {#if errorMessage}
+        <p class="text-3xl font-semibold mb-4">Error</p>
+        <p class="text-xl">
+          {errorMessage}
+        </p>
+      {:else}
+        <p class="text-3xl font-semibold mb-4">Valid 3D model file URL required</p>
+        <p class="text-xl">
+          Please provide a valid 3D model file URL via query parameter <code
+            class="bg-source-200 dark:bg-source-800 text-source-800 dark:text-source-200 p-1 rounded-sm"
+          >
+            url
+          </code>. Supported model formats include GLB, GLTF, OBJ, and STL. Maximum allowed file
+          size is {MAX_CONTENT_LENGTH_MB} MB.
+        </p>
+      {/if}
     </div>
   </div>
 {/if}
